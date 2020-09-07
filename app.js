a9489d3dff5c2b49c4ecf4f4fd2e4976fb004816
cshorter-tech <13804036+cshorter-tech@users.noreply.github.com>
2020-09-07 18:29:18 -0400

    Added HTTPS GET request to App Server

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const https = require('https');
+
 const app = express();
+const location = "London";
+const key = ""
+const url = `https://api.openweathermap.org/data/2.5/weather${location}?q=${location}&appid=${key}`
+
 
 app.get('/', function (req, res) {
+    https.get(url, function (response) {
+        console.log(response);
+    })
     res.send("Hello World")
 })
 
 app.listen('3000', function () {
-    console.log("Server is running on localhost 3000..")
+    console.log(`Server is running on port 3000..`)
 })
