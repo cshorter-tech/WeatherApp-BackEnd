7697b70327123675731c25a01d59a73fbfcf4bda
cshorter-tech <13804036+cshorter-tech@users.noreply.github.com>
2020-09-07 19:52:27 -0400

    Set up HTTPS NODE to parse JSON Requests

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ require('dotenv').config();
 const app = express();
 const location = "London";
 const key = process.env.NODE_APP_WEATHER_API_KEY
-const url = `https://api.openweathermap.org/data/2.5/weather${location}?q=${location}&appid=${key}`
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`
 
 
 app.get('/', function (req, res) {
     https.get(url, function (response) {
-        console.log(response);
+        console.log('statusCode:', response.statusCode);
+        response.on('data', function (data) {
+            console.log(JSON.parse(data).weather[0].description);
+        })
     })
     res.send("Hello World")
 })
 
 app.listen('3000', function () {
     console.log(`Server is running on port 3000..`)
+    // console.log(url)
 })
