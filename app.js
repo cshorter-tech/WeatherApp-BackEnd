b8e9ccc736ea9f500208d4175fcb5501c4680aa9
cshorter-tech <13804036+cshorter-tech@users.noreply.github.com>
2020-09-08 00:00:44 -0400

    Created front end routing to handle body parsing from form into post method to root

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const location = "North Carolina";
-const key = process.env.NODE_APP_WEATHER_API_KEY
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=Imperial`
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post('/', function (req, res) {
+    // console.log();
+    const queryLocation = req.body.cityName;
+    const key = process.env.NODE_APP_WEATHER_API_KEY
+    const units = "Imperial"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${queryLocation}&appid=${key}&units=${units}`
+    console.log("Post message recieved.")
     https.get(url, function (response) {
         console.log('statusCode:', response.statusCode);
         response.on('data', function (data) {
@@ -17,7 +26,7 @@ app.get('/', function (req, res) {
             const weatherDescription = weatherData.weather[0].description
             const iconCode = weatherData.weather[0].icon
             const imageURL = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
-            res.write(`<h1>The temperature in ${location} is ${temp}&degF </h1>`);
+            res.write(`<h1>The temperature in ${queryLocation} is ${temp}&degF </h1>`);
             res.write(`<p>The weather is currently ${weatherDescription}.<p>`);
             res.write(`<img src="${imageURL}" alt="image blank"></img>`);
             res.send();
@@ -25,6 +34,11 @@ app.get('/', function (req, res) {
     })
     // res.send("Hello World")
 })
+
+
+
+
+
 
 app.listen('3000', function () {
     console.log(`Server is running on port 3000..`)
