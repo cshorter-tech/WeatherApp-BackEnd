b0c4b790d675de88f1f9a9b7da3e8108fadd67e9
cshorter-tech <13804036+cshorter-tech@users.noreply.github.com>
2020-09-07 22:38:19 -0400

    added weather elements to routing

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ const https = require('https');
 require('dotenv').config();
 
 const app = express();
-const location = "London";
+const location = "North Carolina";
 const key = process.env.NODE_APP_WEATHER_API_KEY
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=Imperial`
 
 
 app.get('/', function (req, res) {
     https.get(url, function (response) {
         console.log('statusCode:', response.statusCode);
         response.on('data', function (data) {
-            console.log(JSON.parse(data).weather[0].description);
+            const weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp
+            const weatherDescription = weatherData.weather[0].description
+            const iconCode = weatherData.weather[0].icon
+            const imageURL = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
+            res.write(`<h1>The temperature in ${location} is ${temp}&degF </h1>`);
+            res.write(`<p>The weather is currently ${weatherDescription}.<p>`);
+            res.write(`<img src="${imageURL}" alt="image blank"></img>`);
+            res.send();
         })
     })
-    res.send("Hello World")
+    // res.send("Hello World")
 })
 
 app.listen('3000', function () {
